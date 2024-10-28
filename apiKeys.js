0b94944f9352563517c9737e41bb0a18e5f96204
Jyotsna  Chaudhari <144878716+chaudharijyotsna65@users.noreply.github.com>
2024-10-28 09:27:39 +0530

    Add files via upload

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "ca1b0b03b98dbe330249e1bce63532b4",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
